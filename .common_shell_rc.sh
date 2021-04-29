4e437485a070d86ff50bc81b3ba244972cc2a04d
narayan <narayanspai2000@gmail.com>
2021-04-29 11:55:14 +0530

    Add bash, zsh config files

---
+++ .common_shell_rc.sh
@@ -0,0 +1,20 @@
+
+if [ -f ~/.aliases ]; then
+    . ~/.aliases
+fi
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+export PATH="$HOME/bin:$PATH"
+export PATH="$HOME/fabric-samples/bin:$PATH"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
